1df1dc2e6089ee7fdb49f6224b1ff44f288f82bd
Volkov Aleksei <rohtuasad@gmail.com>
2021-02-07 23:35:28 +0300

    init application

---
+++ services/src/main/kotlin/ru/rohtuasad/securityutils/SecurityUtilsApplication.kt
@@ -0,0 +1,11 @@
+package ru.rohtuasad.securityutils
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityUtilsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecurityUtilsApplication>(*args)
+}
---
+++ services/src/test/kotlin/ru/rohtuasad/securityutils/SecurityUtilsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.rohtuasad.securityutils
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityUtilsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
