f2d58b3a995ed5c7800c172e8f7244cc0444cf37
Volkov Aleksei <rohtuasad@gmail.com>
2022-03-16 20:37:28 +0300

    Added user service layer

--- services/src/main/kotlin/ru/rohtuasad/securityutils/user/controller/UserController.kt
+++ services/src/main/kotlin/ru/rohtuasad/securityutils/user/controller/UserController.kt
@@ -1,28 +1,26 @@
 package ru.rohtuasad.securityutils.user.controller
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import ru.rohtuasad.securityutils.user.model.User
-import ru.rohtuasad.securityutils.user.repository.UserRepository
+import ru.rohtuasad.securityutils.user.service.UserService
 import java.util.*
 
 @RestController
 @RequestMapping("/v1")
-class UserController(val userRepository: UserRepository) {
+class UserController(val userService: UserService) {
     @GetMapping("/user/{id}")
-    fun getUserProfile(@PathVariable id: UUID): ResponseEntity<Optional<User>> {
-        return ResponseEntity(userRepository.findById(id), HttpStatus.OK)
+    fun getUserProfile(@PathVariable id: UUID): ResponseEntity<User> {
+        return ResponseEntity.ok(userService.getUserProfile(id))
     }
 
     @GetMapping("/user")
-    fun getUserProfile(): MutableIterable<User> {
-        return userRepository.findAll()
+    fun getUserProfile(): ResponseEntity<MutableIterable<User>> {
+        return ResponseEntity.ok(userService.getUserProfile())
     }
 
     @PostMapping("/user")
-    fun registerUser(@RequestBody user: User) {
-        user.setEncodedPassword(user.password)
-        userRepository.save(user)
+    fun registerUser(@RequestBody user: User): ResponseEntity<User> {
+        return ResponseEntity.ok(userService.registerUser(user))
     }
 }
---
+++ services/src/main/kotlin/ru/rohtuasad/securityutils/user/service/UserService.kt
@@ -0,0 +1,26 @@
+package ru.rohtuasad.securityutils.user.service
+
+import org.springframework.stereotype.Service
+import ru.rohtuasad.securityutils.user.model.User
+import ru.rohtuasad.securityutils.user.repository.UserRepository
+import java.util.*
+
+@Service
+class UserService(val userRepository: UserRepository) {
+    fun getUserProfile(id: UUID): User {
+        val user = userRepository.findById(id)
+        if (user.isPresent) {
+            return user.get()
+        }
+        throw RuntimeException("Не удалось найти пользователя с uuid = $id")
+    }
+
+    fun getUserProfile(): MutableIterable<User> {
+        return userRepository.findAll()
+    }
+
+    fun registerUser(user: User): User {
+        user.setEncodedPassword(user.password)
+        return userRepository.save(user)
+    }
+}
