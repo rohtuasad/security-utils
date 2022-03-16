abacb358993b5a0f531d4e2b59cda4811e211517
Volkov Aleksei <rohtuasad@gmail.com>
2022-03-16 20:42:45 +0300

    Fixed password field length

--- services/src/main/resources/db/migration/V1.2__security-utils.user.sql
+++ services/src/main/resources/db/migration/V1.2__security-utils.user.sql
@@ -1,14 +1,14 @@
 create table security_utils.user
 (
-    user_id uuid
+    user_id  uuid
         constraint user_id_pk
             primary key,
-    login       varchar(50)
+    login    varchar(50)
         constraint login_uk
             unique,
-    email varchar(50)
+    email    varchar(50)
         constraint email_uk
             unique,
-    name varchar(50),
-    password        varchar(50)
+    name     varchar(50),
+    password varchar(60)
 );
