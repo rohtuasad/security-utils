cece1951b99d65918479c131138431132263d6a7
Volkov Aleksei <rohtuasad@gmail.com>
2022-04-06 23:04:41 +0300

    Updated User class

--- services/src/main/java/ru/rohtuasad/securityutils/user/model/User.java
+++ services/src/main/java/ru/rohtuasad/securityutils/user/model/User.java
@@ -2,8 +2,10 @@ package ru.rohtuasad.securityutils.user.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
-import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -21,13 +23,19 @@ public class User implements UserDetails {
   @Column("USER_ID")
   private UUID id;
   private final String name;
+  @NotNull
+  @NotEmpty
   private final String login;
+  @NotNull
+  @NotEmpty
   private String password;
+  @NotNull
+  @NotEmpty
   private final String email;
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    throw new UnsupportedOperationException();
+    return List.of();
   }
 
   @Override
