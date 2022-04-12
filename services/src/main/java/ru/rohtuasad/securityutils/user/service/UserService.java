5067dfb77a2ef305c4064cc7a6f9014934045397
Volkov Aleksei <rohtuasad@gmail.com>
2022-04-12 23:48:28 +0300

    Fixed UserService annotation

--- services/src/main/java/ru/rohtuasad/securityutils/user/service/UserService.java
+++ services/src/main/java/ru/rohtuasad/securityutils/user/service/UserService.java
@@ -1,8 +1,10 @@
 package ru.rohtuasad.securityutils.user.service;
 
+import org.springframework.stereotype.Service;
 import ru.rohtuasad.securityutils.user.controller.UserAlreadyExistException;
 import ru.rohtuasad.securityutils.user.model.User;
 
+@Service
 public interface UserService {
   User registerUser(User userDto) throws UserAlreadyExistException;
 }
